1b78fb3dedea8de44591bfed4f7c657ac3175f0d
warrenfalk <warren@warrenfalk.com>
2014-01-09 12:06:45 -0500

    alpha

---
+++ MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace QueryToExcel
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.queryTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.connectionDropdown = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // queryTextBox
+            // 
+            this.queryTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.queryTextBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.queryTextBox.Location = new System.Drawing.Point(0, 80);
+            this.queryTextBox.Multiline = true;
+            this.queryTextBox.Name = "queryTextBox";
+            this.queryTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.queryTextBox.Size = new System.Drawing.Size(842, 421);
+            this.queryTextBox.TabIndex = 0;
+            this.queryTextBox.WordWrap = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(842, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excelToolStripMenuItem});
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.exportToolStripMenuItem.Text = "E&xport";
+            // 
+            // excelToolStripMenuItem
+            // 
+            this.excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            this.excelToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.excelToolStripMenuItem.Text = "To &Excel";
+            this.excelToolStripMenuItem.Click += new System.EventHandler(this.excelToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.connectionDropdown);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 24);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(842, 56);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // connectionDropdown
+            // 
+            this.connectionDropdown.FormattingEnabled = true;
+            this.connectionDropdown.Location = new System.Drawing.Point(79, 19);
+            this.connectionDropdown.Name = "connectionDropdown";
+            this.connectionDropdown.Size = new System.Drawing.Size(192, 21);
+            this.connectionDropdown.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "&Connection";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 501);
+            this.Controls.Add(this.queryTextBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Query To Excel";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox queryTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox connectionDropdown;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,134 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace QueryToExcel
+{
+    public partial class MainForm : Form
+    {
+        ConnectionInfo[] Connections = new ConnectionInfo[] {
+            new ConnectionInfo {
+                Name = "DIV",
+                Caption = "GP (DIV)",
+                Database = "DIV",
+                Server = @"DIVSQL3\GP",
+            },
+            new ConnectionInfo {
+                Name = "Divisions_Inc_MSCRM",
+                Caption = "CRM (Divisions_Inc_MSCRM)",
+                Database = "Divisions_Inc_MSCRM",
+                Server = @"DIVSQL3\CRM",
+            },
+        };
+
+        class ConnectionInfo
+        {
+            public string Name { get; set; }
+            public string Caption { get; set; }
+            public string Database { get; set; }
+            public string Server { get; set; }
+
+            public string ConnectionString
+            {
+                get
+                {
+                    return string.Format(@"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=True;Initial Catalog={2};Data Source={1};Use Procedure for Prepare=1;Auto Translate=True;Packet Size=4096;Use Encryption for Data=False;Tag with column collation when possible=False", Name, Server, Database);
+                }
+            }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        public MainForm()
+        {
+            // TODO: initialize form with contents of clipboard
+            InitializeComponent();
+            connectionDropdown.Items.AddRange(Connections);
+        }
+
+        private void excelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // basically zip up everything in the template directory, doing template replacement in appropriate places
+            using (ZipFile zip = new ZipFile())
+            {
+                // TODO: fix this, load from resource or something
+                Add(zip, @"C:\Users\wfalk\source\QueryToExcel\Template", "", delegate(string sourcePath, string targetFolder)
+                {
+                    if (targetFolder == "xl" && Path.GetFileName(sourcePath) == "connections.xml")
+                    {
+                        ConnectionInfo ci = (ConnectionInfo)connectionDropdown.SelectedItem;
+
+                        XmlDocument doc = new XmlDocument();
+                        doc.Load(sourcePath);
+                        XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+                        nsmgr.AddNamespace("x", doc.DocumentElement.GetAttribute("xmlns"));
+
+                        XmlElement c = (XmlElement)doc.SelectSingleNode("/x:connection");
+                        c.SetAttribute("name", ci.Name);
+                        c.SetAttribute("description", ci.Name);
+                        
+                        XmlElement dbPr = (XmlElement)doc.SelectSingleNode("/x:connections/x:connection/x:dbPr", nsmgr);
+                        dbPr.SetAttribute("command", CommandTextToAttribute(queryTextBox.Text));
+                        dbPr.SetAttribute("connection", ci.ConnectionString);
+                        MemoryStream file = new MemoryStream();
+                        doc.Save(file);
+                        file.Position = 0;
+                        return file;
+                    }
+                    return null;
+                });
+                // TODO: fix this, save to different filename if first exists already
+                zip.Save(@"C:\Users\wfalk\Documents\MyZipFile.zip");
+            }
+        }
+
+        private readonly static Regex CommandTextParse = new Regex(@"[\r\n]", RegexOptions.Compiled | RegexOptions.Multiline);
+        private string CommandTextToAttribute(string sql)
+        {
+            return CommandTextParse.Replace(sql, delegate(Match match)
+            {
+                string replacement = String.Format("_x{0:x4}_", (int)match.Value[0]);
+                return replacement;
+            });
+        }
+
+        private void Add(ZipFile zip, string sourcePath, string targetFolder, Func<string, string, Stream> hook)
+        {
+            if (Directory.Exists(sourcePath))
+            {
+                foreach (string file in Directory.GetFiles(sourcePath))
+                    Add(zip, file, targetFolder, hook);
+                foreach (string file in Directory.GetDirectories(sourcePath))
+                    Add(zip, file, Path.Combine(targetFolder, Path.GetFileName(file)), hook);
+                return;
+            }
+            string target = Path.Combine(targetFolder, Path.GetFileName(sourcePath));
+            Stream stream;
+            stream = hook(sourcePath, targetFolder);
+            try
+            {
+                if (stream == null)
+                    stream = File.OpenRead(sourcePath);
+                zip.AddEntry(target, stream);
+            }
+            finally
+            {
+                //if (stream != null)
+                    //stream.Dispose();
+            }
+        }
+    }
+}
